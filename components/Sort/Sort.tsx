faaf537b13d07acb25231fd2518880b6e359f8e4
Kristakri <nago.gun@mail.ru>
2023-09-19 11:10:27 +0300

    Доступность сортировки с клавиатуры

--- components/Sort/Sort.tsx
+++ components/Sort/Sort.tsx
@@ -1,6 +1,7 @@
 import { SortEnum, SortProps } from "./SortProps";
 import styles from "./Sort.module.css";
 import cn from "classnames";
+import { KeyboardEvent } from 'react';
 
 export const Sort = ({sort, setSort, className, ...props}: SortProps):JSX.Element => {
   return (
@@ -10,6 +11,8 @@ export const Sort = ({sort, setSort, className, ...props}: SortProps):JSX.Elemen
         className={cn({
           [styles.isActive]: sort == SortEnum.Rating
         })}
+        tabIndex={0}
+        onKeyDown={(e: KeyboardEvent) => { if(e.code == "Space" || e.code == "Enter") setSort(SortEnum.Rating); }}
       >
         <span className={cn('material-symbols-rounded', styles.sortIcon)}>sort</span>По рейтингу
       </span>
@@ -19,6 +22,8 @@ export const Sort = ({sort, setSort, className, ...props}: SortProps):JSX.Elemen
         className={cn({
           [styles.isActive]: sort == SortEnum.Price
         })}
+        tabIndex={0}
+        onKeyDown={(e: KeyboardEvent) => { if(e.code == "Space" || e.code == "Enter") setSort(SortEnum.Price); }}
       >
         <span className={cn('material-symbols-rounded', styles.sortIcon)}>sort</span>По цене
       </span>
