589877363247bb302c29d13426d805878a310829
Kristakri <nago.gun@mail.ru>
2023-09-19 09:53:49 +0300

    Доступность с клавиатуры

--- layout/Layout.tsx
+++ layout/Layout.tsx
@@ -4,16 +4,35 @@ import cn from "classnames";
 import { Header } from "./Header/Header";
 import { Sidebar } from "./Sidebar/Sidebar";
 import { Footer } from "./Footer/Footer";
-import { Component, FunctionComponent } from "react";
+import { Component, FunctionComponent, useState, KeyboardEvent, useRef } from "react";
 import { AppContextProvider, IAppContext } from "../context/app.context";
 import { Up } from "../components/components";
 
 const Layout = ({ children }: LayoutProps):JSX.Element => {
+  const [isSkipLinkDisplayed, setIsSkipLinkDisplayed] = useState<boolean>(false);
+  const bodyRef = useRef<HTMLDivElement>(null);
+
+  const skipContentAction = (key: KeyboardEvent) => {
+    if(key.code == "Space" || key.code == "Enter") {
+      key.preventDefault();
+      bodyRef.current?.focus();
+    }
+    setIsSkipLinkDisplayed(false);
+  };
+
   return (
     <div className={styles.wrapper}>
+      <a 
+        tabIndex={1} 
+        onFocus={() => setIsSkipLinkDisplayed(true)}
+        className={cn(styles.skipLink, {
+          [styles.displayed]: isSkipLinkDisplayed
+        })}
+        onKeyDown={skipContentAction}
+      >Сразу к содержанию</a>
       <Header className={styles.header}/>
       <Sidebar className={styles.sidebar}/>
-      <main className={styles.main}>
+      <main className={styles.main} ref={bodyRef} tabIndex={0}>
         {children}
         <Up />
       </main>
