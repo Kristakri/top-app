39df2dd649c144ab937d00f812f56d4f82d978de
Kristakri <nago.gun@mail.ru>
2023-09-20 10:36:30 +0300

    Добавление Яндекс метрики

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,20 +2,34 @@ import { AppProps } from 'next/dist/next-server/lib/router/router';
 import '../styles/globals.css';
 import Head from 'next/head';
 import router from 'next/router';
+import ym from 'react-yandex-metrika';
+import { YMInitializer } from 'react-yandex-metrika';
 
 function MyApp({ Component, pageProps }: AppProps): JSX.Element {
+	router.events.on('routerChangeComplete', (url: string) => {
+		if(typeof window !== 'undefined') {
+			ym('hit', url);
+		}
+	});
+
 	return <>
 		<Head>
 			<title>NoCringe</title>
 			<link rel="icon" href="/favicon.ico" />
-			<link rel="preconnect" href="https://fonts.googleapis.com" />
 			<link rel="preconnect" href="https://fonts.gstatic.com" />
+			<link rel="preconnect" href="https://mc.yandex.ru" />
+			<link rel="preconnect" href="https://fonts.googleapis.com" />
 			<link href="https://fonts.googleapis.com/css2?family=Noto+Sans:wght@300;400;500;700&display=swap" rel="stylesheet"/>
 			<link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Rounded:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" />
 			<link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" />
 			<meta property="og:url" content={process.env.NEXT_PUBLIC_DOMAIN + router.asPath} />
 			<meta property="og:locale" content="ru_RU" />
 		</Head>
+		<YMInitializer 
+			accounts={[]}
+			options={{ webvisor: true, defer: true }}
+			version="2"
+		/>
 		<Component {...pageProps} />
 	</>;
 }
